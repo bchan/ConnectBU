a44185df6eed20e110e312404475bef99da9acda
Ben Chan <chanben@bu.edu>
2020-11-06 19:51:53 -0500

    Added font and changed navbar colors around

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,6 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { useLocation } from 'react-router-dom';
 import { useScrollTrigger } from '@material-ui/core';
 import { useStyles } from '../styles/NavBar.styles';
 import { Link } from 'react-router-dom';
@@ -25,43 +26,46 @@ import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 import { useSelector, useDispatch } from 'react-redux';
 import { login, logout, selectLoginState } from '../redux/loginSlice';
 
+
+const menuItems = [
+  {
+    title: 'Home',
+    icon: <HomeIcon />,
+    path: '/',
+  },
+  {
+    title: 'Features',
+    icon: <DescriptionIcon />,
+    path: '/',
+  },
+  {
+    title: 'Community',
+    icon: <PeopleIcon />,
+    path: '/',
+  },
+  {
+    title: 'Support',
+    icon: <ContactSupportIcon />,
+    path: '/support',
+  },
+]
+
 export default function NavBar() {
+  const location = useLocation();
   const classes = useStyles();
   let trigger = useScrollTrigger(
     {
       disableHysteresis: true,
       threshold: 100
     }
-  )
+  );
   const [state, setState] = React.useState({
     isDrawerOpen: false,
   });
 
   const isLoggedIn = useSelector(selectLoginState);
   const dispatch = useDispatch();
-
-  const menuItems = [
-    {
-      title: 'Home',
-      icon: <HomeIcon />,
-      path: '/',
-    },
-    {
-      title: 'Features',
-      icon: <DescriptionIcon />,
-      path: '/',
-    },
-    {
-      title: 'Community',
-      icon: <PeopleIcon />,
-      path: '/',
-    },
-    {
-      title: 'Support',
-      icon: <ContactSupportIcon />,
-      path: '/support',
-    },
-  ]
+  const shouldUseScroll = location.pathname === '/';
 
   const menuList = (
     <List className={classes.list}>
@@ -84,8 +88,14 @@ export default function NavBar() {
     setState({isDrawerOpen: open});
   }
 
-  if (window.location.pathname !== '/') {
-    trigger = true;
+  let responseSuccess = (event) => {
+    dispatch(login());
+  }
+
+  let responseError = (event) => {
+    if (event.error !== "popup_closed_by_user") {
+      alert(event.error);
+    }
   }
 
   return (
@@ -96,19 +106,19 @@ export default function NavBar() {
       >
         {menuList}
       </Drawer>
-      <AppBar className={trigger? classes.scrolledBar : classes.bar}> 
+      <AppBar className={(!shouldUseScroll)? classes.solidBar : (trigger)? classes.scrolledBar : classes.bar} elevation={0}> 
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => {toggleDrawer(true)}}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            ConnectBU
+            Connect<a style={{color: '#CC0000'}}>BU</a>
           </Typography>
           <GoogleLogin 
                 clientId="575450034905-v02tn4l35jt2s3mhd46impe7pb79cc18.apps.googleusercontent.com"
                 // buttonText="Sign In with BU Account"
-                // onSuccess={responseGoogle}
-                // onFailure={responseGoogle}
+                onSuccess={responseSuccess}
+                onFailure={responseError}
                 cookiePolicy={'single_host_origin'}
                 style={{disabled: 'false'}}
                 render={(renderProps) => (
