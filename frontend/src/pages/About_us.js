4ebaeaa37fbd9404dad41f7391d095e17f53b972
hussainb2030 <hussainb@bu.edu>
2020-11-12 20:06:48 +0300

    About us
    I added pictures and stuff to complete the about us page

--- frontend/src/pages/About_us.js
+++ frontend/src/pages/About_us.js
@@ -1,49 +1,155 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
+import pic1 from '../images/Hussain.jpg';
+import pic2 from '../images/Yousuf.jpg';
+import pic3 from '../images/Ben.jpg';
+import pic4 from '../images/Nadim.jpg';
+import pic5 from '../images/Damani.jpg';
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(0),
-    marginTop: theme.spacing(0),
-    marginLeft: theme.spacing(20),
-    marginBottom: theme.spacing(10),
-    minWidth: 150,
+  screen: {
+    paddingLeft: 50, 
+    backgroundColor: "rgb(240,240,240)"
   },
-  selectEmpty: {
-    marginTop: theme.spacing(0),
+  boxes: {
+    border: "1px solid grey", 
+    padding: 30, 
+    width: "70%",
+    borderRadius: 10, 
+    backgroundColor: "rgb(255,255,255)", 
+    marginBottom: 10,
+    marginTop: 10
   },
+  image: {
+    width: '20%', 
+    borderRadius: '80%', 
+    marginRight: 50
+  }, 
+  separation: {
+    width: "100%", 
+    height: 0, 
+    borderTop: "1px solid grey", 
+    borderColor: "grey", 
+    marginTop: 10, 
+    marginBottom: 20
+  },
+  editButton: {
+    backgroundColor: '#EB5757',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#B03E3E',
+      color: 'white'
+    },
+    marginLeft: 10
+  }
 }));
+
+
 export default function Signup()  {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  return (
 
-    return (
+      <div className={classes.screen}>
 
-        <div>
-        <p style={{
-            'white-space': 'pre-wrap'
-            }}>{"\n"}
-        </p>
-        <p style={{
-            'white-space': 'pre-wrap'
-            }}>{"\n"}
-        </p>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
 
-            <Grid item xs={10} sm={5}>
-                <div className={classes.paper}>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          className={classes.boxes}>
+          
+          <img src={pic1} alt="Logo" className={classes.image} />
+          <Grid justify="flex-start">
+            <h1>Hussain Albayat</h1>
+            <p>ENG - Electrical Engineering - May 2021</p>
+            <p>I like to learn new and hard skills like swimming and biking</p>
+          </Grid>
 
-                 <h2>Who We Are?</h2>
+        </Grid>
 
-                 </div>
-            </Grid>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          className={classes.boxes}>
+          
+          <img src={pic2} alt="Logo" className={classes.image} />
+          <Grid justify="flex-start">
+            <h1>Yousuf Baker</h1>
+            <p>ENG - Electrical Engineering - May 2021</p>
+            <p>I can clab with one hand</p>
+          </Grid>
+
+        </Grid>
+
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          className={classes.boxes}>
+          
+          <img src={pic3} alt="Hussain" className={classes.image} />
+          <Grid justify="flex-start">
+            <h1>Benjamin Chan</h1>
+            <p>ENG - Computer Engineering - May 2021</p>
+            <p>I like to learn new and hard skills like swimming and biking</p>
+          </Grid>
+
+        </Grid>
+
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          className={classes.boxes}>
+          
+          <img src={pic4} alt="Logo" className={classes.image} />
+          <Grid justify="flex-start">
+            <h1>Nadim El Helou</h1>
+            <p>ENG - Computer Engineering - May 2021</p>
+            <p>waiting for their input</p>
+          </Grid>
+
+        </Grid>
+
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+        <p style={{'white-space': 'pre-wrap'}}>{"\n"}</p>
+
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          className={classes.boxes}>
+          
+          <img src={pic5} alt="Logo" className={classes.image} />
+          <Grid justify="flex-start">
+            <h1>Damani Philip</h1>
+            <p>ENG - Computer Engineering - May 2021</p>
+            <p>waiting for their input</p>
+          </Grid>
+
+        </Grid>
         </div>
-    )
+  );
 }
