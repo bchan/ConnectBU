685335911ae932b33737801e27ce827fd064977f
d-philip <djphilip@bu.edu>
2020-12-06 16:14:59 -0500

    Added basic profile retrieval API.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Student, Class, Club, Lab, CampusJob, TakesClass, TakesJob, JoinsClub, JoinsLab
@@ -8,14 +8,28 @@ api = Api(app)
 app.config.from_pyfile('config.py')
 db.init_app(app)
 
+print(Student.__table__.columns)
 
-class GetUser(Resource):
-    def get(self, user):
-        res = Student.query.filter_by(first_name=user).first()
-        return {'first_name': res.first_name}
+class GetProfile(Resource):
+    def get(self, email):
+        user = Student.query.filter_by(email=email).first()
+
+        if (user is None):
+            return {'error': 'User could not be found.'}, 404
+        else:
+            user_info = {
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'major': user.major,
+                'minor': user.minor,
+                'year': user.school_year,
+                'has_ipad': user.has_ipad
+            }
+            return user_info, 200
 
 # API Routes
-api.add_resource(GetUser, '/<string:user>')
+api.add_resource(GetProfile, '/profile/<string:email>')
 
 if __name__ == '__main__':
     app.run(debug=True)
