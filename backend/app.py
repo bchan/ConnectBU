84a690e95e9ec73a734a1f394afd3fc50d59d6b2
d-philip <djphilip@bu.edu>
2020-12-06 21:26:02 -0500

    Restructured user sign up endpoint.

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,6 @@ api = Api(app)
 app.config.from_pyfile('config.py')
 db.init_app(app)
 
-
 class GetUser(Resource):
     def get(self, user):
         res = Student.query.filter_by(first_name=user).first()
@@ -18,50 +17,49 @@ class NewUser(Resource):
     def post(self):
 
         json_data = request.get_json(force=True)
+        print(json_data)
 
-        Student.insert().value(email = json_data['email'], 
-                                firstname = json_data['firstName'], lastName =json_data['lastname'],
-                                country = json_data['country'], major1 = json_data['major1'], 
-                                major2 = json_data['major2'], mminor = json_data['minor'])
+        new_student = Student(
+            email = json_data['email'],
+            first_name = json_data['first_name'],
+            last_name =json_data['last_name'],
+            major = json_data['major'],
+            minor = json_data['minor'],
+            school_year = json_data['school_year'],
+            has_ipad = json_data['has_ipad']
+        )
 
-    new_student = Student(email = json_data['email'], 
-                                firstname = json_data['firstName'], lastName =json_data['lastname'],
-                                country = json_data['country'], major1 = json_data['major1'], 
-                                major2 = json_data['major2'], mminor = json_data['minor'])
-
-    db.session.add(new_student)
-
-
-        # research = json_data['research'],
-        #                         class_options = json_data['class_options'], club = json_data['club']
+        db.session.add(new_student)
+        db.session.commit()
 
         listclasses = json_data['class_options']
         listresearch = json_data['research']
         listclubs = json_data['club']
 
-        for i in listclasses:
-             TakesClass.insert().value(Student = email, class = i)
+        for i in range(len(listclasses)):
+            new_takes_class = TakesClass(email = json_data['email'], class_id = i+1)
+            db.session.add(new_takes_class)
 
-        for i in listresearch:
-             JoinsLab.insert().value(Student = email,  lab = i)
+        for i in range(len(listresearch)):
+            new_joins_lab = JoinsLab(email = json_data['email'],  lab_id = i+1)
+            db.session.add(new_joins_lab)
 
-        for i in listclubs:
-            JoinsClub.insert().value(Student = email,  club = i)
+        for i in range(len(listclubs)):
+            new_joins_club = JoinsClub(email = json_data['email'],  club_id = i+1)
+            db.session.add(new_joins_club)
 
+        db.session.commit()
+        res = Student.query.filter_by(email=json_data['email']).first()
 
-        res = Student.query.filter_by(first_name=frst).first()
-
-        if(res.size != 0)
-            return 'user data inserted successfully'
-        else
-            return 'go fuck yourself'
-
-        
+        if(res is not None):
+            return {response: 'User data inserted successfully'}, 200
+        else:
+            return {error: 'Error creating user.'}, 404
 
 
 # API Routes
 api.add_resource(GetUser, '/<string:user>')
-api.add_resource(NewUser, '/users')
+api.add_resource(NewUser, '/create_user')
 
 if __name__ == '__main__':
     app.run(debug=True)
