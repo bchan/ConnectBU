95dda15755953478b322beb3ec977145e736ee52
Ben Chan <chanben@bu.edu>
2020-12-11 13:52:44 -0500

    Adjusted naming for pages -- could not find page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import Support from './pages/Support';
 import HomeLI from './pages/Home_LI'
 import Search from './pages/Search'
 import AboutUs from './pages/AboutUs'
-import SignUp from './pages/SignUp';
+import SignUp from './pages/Signup';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import BotBar from './components/BotBar'
