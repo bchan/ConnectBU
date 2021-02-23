25924c108aa9e508b71e679729affeea59dd794b
ybaker661 <50885811+ybaker661@users.noreply.github.com>
2021-02-22 21:24:51 -0500

    Update search.py

--- backend/search.py
+++ backend/search.py
@@ -21,16 +21,62 @@ es = Elasticsearch(
 
 print('ES is active')
 
-document = {
-            "email" : "ybaker@bu.edu",
-            "first_name" : "yousuf",
-            "last_name" : "baker",
-            "major1" : "EE",
-            "school_year" : "2021",
-            "has_ipad" : "Yes"
-}
 
-es.index(index="stud_profiles", doc_type="_doc", id="1", body=document)
+doc1 = {
+    "name": 'baker, yousuf', 
+    "majors": 'electrical engineering', 
+    "minors": 'bag_getting', 
+    "year": '2021', 
+    "ipad?": 'yes'
+    }
+
+doc2 = {
+    "name": 'philip, damani', 
+    "majors": 'computer', 
+    "minors": 'female anatomy', 
+    "year": '2021', 
+    "ipad?": 'yes'
+    }
+
+
+
+
+res = es.index(index="profiles", id=1, body=doc1)
+res2 = es.index(index="profiles", id=2, body=doc2)
 print('\ndocument ingested...BUUUUUURP\n')
 
-print(es.get(index="stud_profiles", doc_type="_doc", id="1"))
+print(res['result'])
+
+res = es.get(index="profiles", id=1)
+print(res['_source'])
+
+es.indices.refresh(index="profiles")
+
+#searching through one field
+# srch = {
+#   "query": {
+#     "bool": {
+#       "must": {
+#         "match": {
+#           "majors": "electrical",
+#         }
+#       }
+#     }
+#   }
+# }
+
+#searching through multiple fields
+srch = {
+        "query": 
+            {"multi_match": 
+                {"query": "female", "fields": ["majors", "minors"]}
+            }
+        }
+
+#search with key term and no specified field is multimatching w/ all fields
+
+
+resl = es.search(body=srch)
+readableresults = resl["hits"]["hits"]
+print("Got %d Hits:" % resl['hits']['total']['value'])
+print ("query hits:", resl["hits"]["hits"])
