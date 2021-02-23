fa815f69cc78baa70b1fbf8e105b86b71d783700
ybaker661 <ybaker@bu.edu>
2021-02-23 13:23:26 -0500

    Update search_api.py

--- backend/search_api.py
+++ backend/search_api.py
@@ -30,6 +30,29 @@ es = Elasticsearch(
     )
 print('ES is active')
 
+doc1 = {
+    "name": 'Damani Philip', 
+    "majors": 'ENG Computer Engineering', 
+    "minors": 'CAS Astronomy', 
+    "year": '2021', 
+    "ipad?": 'no'
+    }
+
+doc2 = {
+    "name": 'Nadim El Helou', 
+    "majors": ['ENG Computer Engineering', "CAS Pyschology"], 
+    "minors": 'CAS Ancient Greek', 
+    "year": '2021', 
+    "ipad?": 'no'
+    }
+
+
+
+
+res = es.index(index="profiles", id=1, body=doc1)
+res2 = es.index(index="profiles", id=2, body=doc2)
+print('\ndocument ingested...BUUUUUURP\n')
+
 class Search(Resource):
     def post(self):
 
