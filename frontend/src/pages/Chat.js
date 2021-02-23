72476dd73a9a5cb2d29572063ee236f69685d45d
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2021-02-22 22:19:51 -0500

    added breadcrumbs to chat

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { App as SendbirdApp } from "sendbird-uikit";
+import { Link as RouterLink } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
 import "sendbird-uikit/dist/index.css";
 
 import "../styles/Chat.styles.css";
@@ -13,6 +17,11 @@ export default function Chat() {
     <div className="Chat">
 
       <p style={{ 'white-space': 'pre-wrap' }}>{"\n"}</p>
+      <Breadcrumbs aria-label="breadcrumb" style={{marginBottom: 10}}>
+        <Link component={RouterLink} to="/">Home</Link>
+        <Link component={RouterLink} to="/profile">Profile</Link>
+        <Typography color="textPrimary">Chat</Typography>
+      </Breadcrumbs>
       <SendbirdApp appId={appID} userId={userID} nickname={nickname}/>
     </div>
   );
