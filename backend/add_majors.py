97c64d95665c49ce91b5b6349352a2ac4623730c
d-philip <djphilip@bu.edu>
2021-03-25 01:16:08 -0400

    Added majors to DB.

--- backend/add_majors.py
+++ backend/add_majors.py
@@ -1,19 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from config import SQLALCHEMY_DATABASE_URI
-from models import db, Minor
+from models import db, Major
 
 engine = db.create_engine(SQLALCHEMY_DATABASE_URI, {})
 session = Session(engine)
 
-def add_minors():
-    with open('minors.txt', 'r') as file:
+def add_majors():
+    with open('majors.txt', 'r') as file:
         for line in file:
             if line == "\n":
                 continue
-            new_minor = Minor(minor_name=line.strip('\n'))
-            session.add(new_minor)
+            new_major = Major(major_name=line.strip('\n'))
+            session.add(new_major)
     session.commit()
 
 if __name__ == '__main__':
-    add_minors()
+    add_majors()
