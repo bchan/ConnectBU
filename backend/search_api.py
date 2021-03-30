126c0ab994aa76a79c06ab7782067edd74ec8d5d
ybaker661 <ybaker@bu.edu>
2021-03-30 17:10:10 -0400

    Update search_api.py

--- backend/search_api.py
+++ backend/search_api.py
@@ -21,6 +21,7 @@ credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key,
                    region, service, session_token=credentials.token)
 
+#initializing elasticsearch instance
 es = Elasticsearch(
         hosts = [{'host': host, 'port': 443}],
         http_auth = awsauth,
@@ -30,30 +31,29 @@ es = Elasticsearch(
     )
 print('ES is active')
 
-doc1 = {
-    "name": 'Damani Philip',
-    "majors": 'ENG Computer Engineering',
-    "minors": 'CAS Astronomy',
-    "year": '2021',
-    "ipad?": 'no'
-    }
+#sample profiles for elasticsearch
+# doc1 = {
+#     "name": 'Damani Philip',
+#     "majors": 'ENG Computer Engineering',
+#     "minors": 'CAS Astronomy',
+#     "year": '2021',
+#     "ipad?": 'no'
+#     }
 
-doc2 = {
-    "name": 'Nadim El Helou',
-    "majors": ['ENG Computer Engineering', "CAS Pyschology"],
-    "minors": 'CAS Ancient Greek',
-    "year": '2021',
-    "ipad?": 'no'
-    }
+# doc2 = {
+#     "name": 'Nadim El Helou',
+#     "majors": ['ENG Computer Engineering', "CAS Pyschology"],
+#     "minors": 'CAS Ancient Greek',
+#     "year": '2021',
+#     "ipad?": 'no'
+#     }
 
-
-
-
-res = es.index(index="profiles", id=1, body=doc1)
-res2 = es.index(index="profiles", id=2, body=doc2)
-print('\ndocument ingested...BUUUUUURP\n')
+# res = es.index(index="profiles", id=1, body=doc1)
+# res2 = es.index(index="profiles", id=2, body=doc2)
+# print('\ndocument ingested...BUUUUUURP\n')
 
 class Search(Resource):
+    #search request
     def post(self):
 
         json_data = request.get_json(force=True)
@@ -84,26 +84,31 @@ class Search(Resource):
 
         return search_results, 200
 
+    #update profile
     def put(self, email):
-        json_data = request.get_json(force=True)
+        user = request.get_json(force=True)
 
-        if json_data['hasIpad']:
+        if user['hasIpad']:
             ipd = 'yes'
         else:
             ipd = 'no'
-        doc = {
-        "name": json_data['firstName'] + ' ' + json_data['lastName'], 
-        "majors": [json_data['major1'], json_data['major2']], 
-        "minors": [json_data['minor']], 
-        "year": json['schoolYear'], 
-        "ipad?": ipd
-        }
-
+            user_info = {
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'major1': user.major1,
+                'major2': user.major2,
+                'minor': user.minor,
+                'year': user.school_year,
+                'has_ipad': user.has_ipad,
+                'club': [club.club_name for club in clubs],
+                'research': [lab.lab_name for lab in labs],
+                'interests': [interest.interest_name for interest in interests]
+            }
         #in reality, convert id string from DB into UUID 
         res = es.index(index="profiles", id=json_data['index'], body=doc1)
         
         return 'User data updated successfully', 200
-
 
 api.add_resource(Search, '/search')
 
