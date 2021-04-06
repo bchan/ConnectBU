0cc06b8c08e14228153801aa403679ab00d1b6f0
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2021-04-05 20:50:23 -0400

    fetch chat login credentials

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { App as SendbirdApp } from "sendbird-uikit";
 import { Link as RouterLink, useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
 import "sendbird-uikit/dist/index.css";
+import axios from 'axios';
 
 import "../styles/Chat.styles.css";
 import { useSelector, useDispatch } from 'react-redux';
@@ -13,33 +14,36 @@ import { selectLoginState, selectUserEmail, selectProfilePic } from '../redux/lo
 export default function Chat() {
   const history = useHistory();
   const isLoggedIn = useSelector(selectLoginState);
-  if(!isLoggedIn){
+  if (!isLoggedIn) {
     history.push("/")
   }
 
   const appID = "APP ID";
-  const userID = useSelector(selectUserEmail);
-  const nickname = "USER'S FULL NAME";
+  const [userID, setUserID] = useState(useSelector(selectUserEmail));
+  const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    axios.get('/user/' + userID)
+      .then((res) => {
+        let userData = res.data;
+        setNickname(userData.first_name + ' ' + userData.last_name)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  })
 
   return (
     <div>
       <div className="Chat">
-
-      <p style={{ 'white-space': 'pre-wrap' }}>{"\n"}</p>
-      <Breadcrumbs aria-label="breadcrumb" style={{marginBottom: 10}}>
-        <Link component={RouterLink} to="/">Home</Link>
-        <Link component={RouterLink} to="/profile">Profile</Link>
-        <Typography color="textPrimary">Chat</Typography>
-      </Breadcrumbs>
-      <SendbirdApp appId={appID} userId={userID} nickname={nickname}/>
-    </div>
-  
-    
-      
-    
+        <p style={{ 'white-space': 'pre-wrap' }}>{"\n"}</p>
+        <Breadcrumbs aria-label="breadcrumb" style={{ marginBottom: 10 }}>
+          <Link component={RouterLink} to="/">Home</Link>
+          <Link component={RouterLink} to="/profile">Profile</Link>
+          <Typography color="textPrimary">Chat</Typography>
+        </Breadcrumbs>
+        {(nickname === '') ? <div></div> : <SendbirdApp appId={appID} userId={userID} nickname={nickname} />}
       </div>
-
+    </div>
   )
-  
-
 }
